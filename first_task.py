6e862839d629e6c284161da0e5342389dafb774c
Роман <Sudorenkoroma@gmail.com>
2024-01-24 20:55:23 +0200

    First

--- first_task.py
+++ first_task.py
@@ -2,19 +2,15 @@ from datetime import datetime
 
 
 def get_days_from_today(date):
+    today = datetime.today()
     try:
         datetime_object = datetime.strptime(date, '%Y-%m-%d')
-        today = datetime.today()
         date_difference = today - datetime_object
         return date_difference.days
     except ValueError:
-        return "Неправильний формат дати."
+        return "Неправильний формат дати"
 
 
+result = get_days_from_today("2021-10-09")
 
-
-current_date = datetime.today().strftime('%Y-%m-%d')
-user_date = input("Введіть дату в форматі 'РРРР-ММ-ДД': ")
-result = get_days_from_today(user_date)
-
-print(f"Сьогодні {current_date}, кількість днів до {user_date}: {result}")
+print(f"Кількість днів до 2021-10-09: {result} днів")
