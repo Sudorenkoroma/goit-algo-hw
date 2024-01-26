1d2b902fa43b4437e955dd36bf61cb3224580d62
Роман <Sudorenkoroma@gmail.com>
2024-01-26 11:54:19 +0200

    second task fixed

--- second_task.py
+++ second_task.py
@@ -1,11 +1,11 @@
 import random
 def get_numbers_ticket(min_num, max_num, quantity):
-    roll_list = []
+    roll_list = set()
     if min_num < 1 or max_num > 1000:
         return roll_list
-    for roll in range(quantity):
-        num = random.randint(min_num, max_num)
-        roll_list.append(num)
+    while len(roll_list) < quantity:
+        roll_list.add(random.randint(min_num, max_num))
+    roll_list = list(roll_list)
     roll_list.sort()
     return roll_list
 
